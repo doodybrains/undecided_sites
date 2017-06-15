8754214cafdbc2f00f4882190a3eea5ffb6917ac
Emma <doodybrainz@gmail.com>
2017-06-15 17:09:55 -0400

    Remove old port var

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ app.set('port', (process.env.PORT || local));
 app.set('view engine', 'ejs');
 require('./app/routes')(app, {});
 app.listen(app.get('port'), () => {
-  console.log('We are live on ' + port);
+  console.log('We are live on');
 });
