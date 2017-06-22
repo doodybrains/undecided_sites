b61883c5bdea6bbdbbafbb6f42fd21329d1846d2
Emma <doodybrainz@gmail.com>
2017-06-22 17:05:55 -0400

    Links are being rendered into send file

--- app/routes/index.js
+++ app/routes/index.js
@@ -14,51 +14,21 @@ module.exports = function(app, db) {
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
       client.query('INSERT INTO site_table(id, links) VALUES($1, $2) returning id', [req.body.id, req.body.links], function(err, result) {
         done();
-        if(err) { return console.error('error running query', err); }
-
-        var s3 = new aws.S3(),
-          file = "views/send.ejs",
-          result = {
-            error: 0,
-            uploaded: []
-          };
-
-        flow.exec(
-          function() {
-            response.render('send',{links:req.body.links});
-          },
-          function() {
-            fs.readFile("views/send.ejs", this);
-          },
-          function(err, data) {
-            s3.putObject({
-              Bucket: process.env.S3_BUCKET,
-              Key: 'test',
-              Body: data,
-              ContentType: 'text/html',
-            }, this);
-          },
-          function(err, data) {
-            if (err) {
-              console.error('Error : ' + err);
-              result.error++;
-            }
-            result.uploaded.push(JSON.stringify(data));
-            this();
-          },
-          function() {
-            response.render("result", {
-              title: "Upload Result",
-              message: result.error > 0 ? "Something is wroing, Check your server log" : "Success!!",
-              entitiyIDs: result.uploaded
-            });
-          });
+        if (err)
+         { console.error(err); response.send("Error " + err); }
+        else
+        { response.send(req.body.links) }
       });
     });
   });
 
+  app.post('/send', function (req, res) {
+    console.log(req.body.response);
+    app.set('data', req.body.response);
+  });
+
   app.get('/send', function (req, res) {
-    res.render('send');
+    res.render('send', {results: app.get('data')});
   });
 
   app.get('/', function (req, response) {
